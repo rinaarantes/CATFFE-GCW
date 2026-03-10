0e824a90437125589fcbfe38c043c32f5b956be8
rih <carol@Rihanna>
2026-03-09 23:21:21 -0300

    catffe

--- CATFFE/telainicial.js
+++ CATFFE/telainicial.js
@@ -14,3 +14,19 @@ function carrossel(){
 }
 
 setInterval(carrossel, 2500);
+
+
+
+ 
+        window.addEventListener("scroll", function () {
+
+            const nav = document.querySelector("nav");
+
+            if (window.scrollY > 30) {
+                nav.classList.add("scrolled");
+            } else {
+                nav.classList.remove("scrolled");
+            }
+
+        });
+   
